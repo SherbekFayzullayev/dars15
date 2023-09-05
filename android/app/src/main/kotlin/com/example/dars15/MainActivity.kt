9c73da86f619ae7954db4203063f8f3ad96e36b6
SherbekFayzullayev <sherbekcode.2328@gmail.com>
2023-09-05 09:30:43 +0500

    search

---
+++ android/app/src/main/kotlin/com/example/dars15/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dars15
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Scaffold(
+    backgroundColor: Colors.blue,
+    body: Container(
+      
+child: Icon(Icons.search,
+size: 70,
+color: Colors.black,),
+alignment: Alignment.center,
+    ),
+  ),
+));
+}
+ 
